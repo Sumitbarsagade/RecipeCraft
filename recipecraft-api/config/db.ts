4f296d44857e4152eebbb2e79c09cb8477ac2d2d
Sumitbarsagade <sumitbarsagade1507@gmail.com>
2026-05-05 22:15:40 +0530

    initial commit.

---
+++ recipecraft-api/config/db.ts
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config;
+
+const connectDB= async()=>{
+    try{
+      const request = await mongoose.connect(process.env.MONGO_URI);
+    
+    console.log(`MongoDB Connected: ${request.connection.host}`)
+    }
+    catch (error:any){
+      console.error(`Error: ${error.message}`);
+
+    }
+};
+
+
+module.exports = connectDB;
